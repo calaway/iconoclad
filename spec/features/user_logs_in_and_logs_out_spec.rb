95fb3ea107264c0e839fe26ca5da285ca30ccdf5
Calaway <calaway@users.noreply.github.com>
2019-11-09 11:00:51 -0700

    Fix rubocop offenses

--- spec/features/user_logs_in_and_logs_out_spec.rb
+++ spec/features/user_logs_in_and_logs_out_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 feature 'User logs in and logs out', js: true do
   scenario 'they are successfully logged in and out' do
-    Capybara.server_host = "localhost"
-    Capybara.server_port = "61316"
+    Capybara.server_host = 'localhost'
+    Capybara.server_port = '61316'
 
     visit(root_path)
     click_button('Login')
